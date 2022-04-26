efb2d62bfb88d8b33c1fd58f2cab95d515b6f97f
RoxanaLedesma <89870682+RoxanaLedesma@users.noreply.github.com>
2022-04-26 17:01:26 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJEhcrNQyAT7bcou5tlz19dLTNURa1Aa0",
+    authDomain: "biblioteca-digital-5d3b2.firebaseapp.com",
+    projectId: "biblioteca-digital-5d3b2",
+    storageBucket: "biblioteca-digital-5d3b2.appspot.com",
+    messagingSenderId: "889200339385",
+    appId: "1:889200339385:web:7310872d6bed47ffca2e68"
+  };
+  firebase.initializeApp(firebaseConfig)
